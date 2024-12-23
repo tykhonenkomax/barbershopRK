4af45b968d376d35392bbc74aa48438c1abf43ff
Tykhonenko Max <>
2024-12-23 15:40:56 +0200

    fix text style in hero

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -8,11 +8,12 @@ const Hero = () => {
             <div className="hero__bg"></div>
             <div className="hero__wrapper container">
                 <h3 className='hero__subtitle'>- Класичні стрижки & Гоління -</h3>
-                <h1 className='hero__title'>Наші стилі волосся<br/>покращують вашу усмішку.</h1>
-                <p className="hero__descr">Наша перукарня - це територія, створена виключно для чоловіків, які
-                    цінують <br/> преміальну якість, час та бездоганний вигляд.</p>
+                <h1 className='hero__title'>Rock N Roll Hair</h1>
+                <p className="hero__descr">це поєднання старої школи, невимушеної атмосфери <br/> з
+                    ретельно складеним саундтреком.</p>
 
-                <a className="hero__btn" href="https://w.wlaunch.net/c/rock_n_roll_hair/b/4926a617-9693-11ea-8c6f-75bfbdeb2ab4/s">
+                <a className="hero__btn"
+                   href="https://w.wlaunch.net/c/rock_n_roll_hair/b/4926a617-9693-11ea-8c6f-75bfbdeb2ab4/s">
                     <Button text="Записатися"/>
                 </a>
             </div>
