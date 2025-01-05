b45fb636ab867931fb49eb49d5b897cfe67ddd9b
Tykhonenko Max <>
2025-01-05 11:00:34 +0200

    fix some problem

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Navbar } from '../../components';
-import { FaUserTie } from 'react-icons/fa';
-import { useDispatch, useSelector } from "react-redux";
-import { showSidebar } from '../../reducers/sidebarReducer';
+// import { FaUserTie } from 'react-icons/fa';
+// import { useDispatch, useSelector } from "react-redux";
+// import { showSidebar } from '../../reducers/sidebarReducer';
 import { images } from '../../constants';
 import './header.scss';
 
 const Header = () => {
-    const dispatch = useDispatch()
-    const user = useSelector(state => state.user.currentUser)
-    const isAuth = useSelector(state => state.user.isAuth)
+    // const dispatch = useDispatch()
+    // const user = useSelector(state => state.user.currentUser)
+    // const isAuth = useSelector(state => state.user.isAuth)
 
     return (
         <div className="header">
